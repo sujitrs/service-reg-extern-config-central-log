3dd67f8e6d8efd01be8e07b5dd06c40971725b22
sujitrs <suryawanshisujeet@gmail.com>
2019-03-06 14:15:01 +0530

    work completed almost

--- web-eureka-client/src/main/java/org/sj/webeurekaclient/WebEurekaClientApplication.java
+++ web-eureka-client/src/main/java/org/sj/webeurekaclient/WebEurekaClientApplication.java
@@ -12,6 +12,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,6 +20,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import lombok.extern.java.Log;
 
@@ -35,70 +37,119 @@ public class WebEurekaClientApplication {
 
 	
 	@Autowired
-	UserRepo userRepo;
+	Repo repo;
 
 	@GetMapping("/list-users/user/getAllUsers/")
 	public String getAllUsers() {
-		return userRepo.getAllUsers();
+		log.log(Level.INFO, "serving get all users");
+		return repo.getAllUsers();
 	}
 
 	@PostMapping("/create-user/user/addUser")
 	public String addUser(@RequestBody String user) {
-		return userRepo.addUser(user);
+		log.log(Level.INFO, "serving add user {0}",user);
+		return repo.addUser(user);
 	}
 	
-	@GetMapping("/profile-file-upload/user/getUser/{id}")
+	@GetMapping(value= {"/profile-file-upload/user/getUser/{id}","scheme-file-upload/user/getUser/{id}"})
 	public String getUser(@PathVariable (value="id") UUID id) {
-		return userRepo.getUser(id);
+		log.log(Level.INFO, "Getting user with ID: {0}",id);
+		return repo.getUser(id);
 	}
 		
-	@PatchMapping("profile-file-upload/user/updateUser/{id}")
+	@PatchMapping("/profile-file-upload/user/updateUser/{id}")
 	public String updateUser(@PathVariable (value="id") UUID id, @RequestBody String receivedUser) {
-		return userRepo.updateUser(id,receivedUser);
+		log.log(Level.INFO, "Updating user with ID: {0} with details {1}",new Object[] {id,receivedUser});
+		return repo.updateUser(id,receivedUser);
+	}
+	
+	
+	@PostMapping("/profile-file-upload/file/addFile")
+	public UUID storeFile(@RequestBody String fileObj) {
+		log.log(Level.INFO, "Storing File: {0}",fileObj);
+		return repo.storeFile(fileObj);
+	}
+	
+	@GetMapping(value= {"/profile-file-upload/file/getFile/{id}","/list-users/file/getFile/{id}","/list-apps/file/getFile/{id}"})
+	public String getFile(@PathVariable (value = "id") UUID fileId) {
+		log.log(Level.INFO, "Getting File with ID: {0}",fileId);
+		return repo.getFile(fileId);
 	}
 		
-	}; 
+	@PatchMapping("/profile-file-upload/file/removeFile/{id}")
+	public HttpStatus updateFile(@PathVariable (value = "id") UUID fileId) {
+		log.log(Level.INFO, "Removing File with ID: {0}",fileId);
+		return repo.updateFile(fileId);
+	}
 	
-
+		
+	@PostMapping("/scheme-file-upload/app/addApp")
+	String addApp(@RequestBody String apprepo) {
+		log.log(Level.INFO, "Adding Application : {0}",apprepo);
+		return repo.addApp(apprepo);
+	}
+	
+	
+	@GetMapping("/scheme-file-upload/app/getApp/{userid}/{schemeid}")
+	String getApp(@PathVariable(value="userid") UUID userid, @PathVariable(value="schemeid") int schemeid) {
+		log.log(Level.INFO, "Getting Application : User ID: {0} and Scheme ID: {1}",new Object[] {userid,schemeid});
+		return repo.getApp(userid, schemeid);
+	}
+		
+		
+	@GetMapping("/list-apps/app/getAllApp")
+		String getAllApp(){
+		log.log(Level.INFO, "Getting All Application");
+		return repo.getAllApp();
+	}
 	
 	
 	
+	@PatchMapping("/scheme-file-upload/app/updateApp/{userid}/{schemeid}")
+	String updateApp(@PathVariable UUID userid, @PathVariable int schemeid, @RequestBody String apprepo) {
+		log.log(Level.INFO, "Updating Application : User ID: {0} and Scheme ID: {1} with details {2}",new Object[] {userid,schemeid,apprepo});
+		return repo.updateApp(userid, schemeid, apprepo); 
+	}
+}
 	
 	@FeignClient("ms-api-gateway")
-	interface UserRepo {
+	interface Repo {
 		
-		String user="/user";
+		String USER="/user";
+		String APP="/app";
+		String FILE="/file";
 		
-		@GetMapping(user+"/getAllUsers")
+		@GetMapping(USER+"/getAllUsers")
 		String getAllUsers();
 		
-		@PostMapping(value =user+"/addUser", consumes = "application/json")
-		public String addUser(String user); 
+		@PostMapping(value =USER+"/addUser", consumes = "application/json")
+		public String addUser(@RequestBody String user); 
 		
-		@GetMapping(user+"/getUser/{id}")
-		public String getUser(@PathVariable (value="id") UUID id); /*{
-			log.log(Level.INFO, "Get user {0}",id);
-			return userRepo.findById(id).orElseThrow(()->new ResourceNotFoundException("UserRepo","id",id));
-		}*/
+		@GetMapping(USER+"/getUser/{id}")
+		public String getUser(@PathVariable (value="id") UUID id); 
 
-		/*
-		 * @GetMapping("/getAllUsers")
-		 * 
-		 * @ResponseBody public List<UserRepo> getAllUsers() { log.log(Level.INFO,
-		 * "Get All Users" ); return userRepo.findAll(); }
-		 */
-
+		@PatchMapping(value =USER+"/updateUser/{id}", consumes = "application/json")
+		public String updateUser(@PathVariable (value="id") UUID id, @RequestBody String receivedUser);
 		
-		@PatchMapping(value =user+"/updateUser/{id}", consumes = "application/json")
-		public String updateUser(@PathVariable (value="id") UUID id, @RequestBody String receivedUser); /*{
-			log.log(Level.INFO, "Update User with ID {0} and details as {1}",new Object[] {id, receivedUser});
-			UserRepo savedUser=userRepo.findById(id).orElseThrow(()->new ResourceNotFoundException("UserRepo","id",id));
-			savedUser.setPoaFileID(receivedUser.getPoaFileID());
-			savedUser.setPoiFileID(receivedUser.getPoiFileID());
-			userRepo.save(savedUser);
-			return savedUser;
-		}*/
+		@PostMapping(value=APP+"/addApp", consumes = "application/json")
+		public String  addApp(@RequestBody String apprepo);
 		
+		@GetMapping(APP+"/getApp/{userid}/{schemeid}")
+		public String getApp(@PathVariable (value="userid")  UUID userid, @PathVariable (value="schemeid") int schemeid);
 		
+		@GetMapping(APP+"/getAllApp")
+		public String getAllApp();
+		
+		@PatchMapping(value=APP+"/updateApp/{userid}/{schemeid}", consumes = "application/json")
+		public String updateApp(@PathVariable (value="userid") UUID userid, @PathVariable (value="schemeid") int schemeid, @RequestBody String apprepo);
+		
+		@PostMapping(value=FILE+"/addFile", consumes="application/json")
+		public UUID storeFile(@RequestBody String fileObj);
+		
+		@GetMapping(FILE+"/getFile/{id}")
+		public String getFile(@PathVariable (value = "id") UUID fileId);
+		
+		@PatchMapping(FILE+"/removeFile/{id}")
+		public HttpStatus updateFile(@PathVariable (value = "id") UUID fileId);
 	}
 
