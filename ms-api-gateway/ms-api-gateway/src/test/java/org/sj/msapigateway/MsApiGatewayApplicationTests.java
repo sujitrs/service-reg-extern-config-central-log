f3dcf5bb8aef7f86604826b10c2c43064474275f
sujitrs <suryawanshisujeet@gmail.com>
2019-03-06 19:23:40 +0530

    work completed almost;)

---
+++ ms-api-gateway/ms-api-gateway/src/test/java/org/sj/msapigateway/MsApiGatewayApplicationTests.java
@@ -0,0 +1,17 @@
+package org.sj.msapigateway;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MsApiGatewayApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
